15c912fa789129586743f2987fc5daf96b96cd74
James Dizon <dizonjerlin@gmail.com>
2022-10-05 21:41:44 -0700

    Commpleted Form Contact

--- js/main.js
+++ js/main.js
@@ -44,87 +44,86 @@ $(function(){
 	 /* Contact form ajax Handler
     ================================================*/
 
-    $(".ajax-form").on('submit', function() {
-    	var form = $(this);
-        var formURL = $(this).attr("action");
-        var postData = $(this).serializeArray();
+    // $(".ajax-form").on('submit', function() {
+    // 	var form = $(this);
+    //     var formURL = $(this).attr("action");
+    //     var postData = $(this).serializeArray();
 
-        $.ajax({
-            url: formURL,
-            type: 'POST',
-            data: postData,
-            dataType: 'json',
+    //     $.ajax({
+    //         url: formURL,
+    //         type: 'POST',
+    //         data: postData,
+    //         dataType: 'json',
 
-            success:function(data, textStatus, jqXHR){
-				console.log("I'm here");
-				console.log(data);
-                if(data.success==1){
+    //         success:function(data, textStatus, jqXHR){
 
-                    form.find(".alert").fadeOut();
-                    form.find(".alert-success").html(data.message);
-                    form.find(".alert-success").fadeIn(600);
-                    console.log(data);
+    //             if(data.success==1){
 
-                }else{
+    //                 form.find(".alert").fadeOut();
+    //                 form.find(".alert-success").html(data.message);
+    //                 form.find(".alert-success").fadeIn(600);
+                    
 
-                	form.find(".alert").fadeOut();
-                    form.find(".alert-danger").html(data.message);
-                    form.find(".alert-danger").fadeIn(600);
+    //             }else{
 
-                }
-            },
+    //             	form.find(".alert").fadeOut();
+    //                 form.find(".alert-danger").html(data.message);
+    //                 form.find(".alert-danger").fadeIn(600);
 
-            error: function(jqXHR, textStatus, errorThrown)  { 
+    //             }
+    //         },
+
+    //         error: function(jqXHR, textStatus, errorThrown)  { 
                 
-                console.log(errorThrown);
-            }
+    //             console.log(errorThrown);
+    //         }
 
-        });
+    //     });
             
 
-        return false;
-     })
+    //     return false;
+    //  })
 
 
 
-    /*
-	On scroll animations
-	================================================
-	*/
+    // /*
+	// On scroll animations
+	// ================================================
+	// */
 
 
-    var $elems = $('.animate-onscroll');
+    // var $elems = $('.animate-onscroll');
 
-    var winheight = $(window).height();
-    var fullheight = $(document).height();
+    // var winheight = $(window).height();
+    // var fullheight = $(document).height();
  
-    $(window).scroll(function(){
-        animate_elems();
-    });
+    // $(window).scroll(function(){
+    //     animate_elems();
+    // });
 
 
 
-    function animate_elems() {
+    // function animate_elems() {
 
-	    wintop = $(window).scrollTop(); // calculate distance from top of window
+	//     wintop = $(window).scrollTop(); // calculate distance from top of window
 	 
-	    // loop through each item to check when it animates
-	    $elems.each(function(){
+	//     // loop through each item to check when it animates
+	//     $elems.each(function(){
 	    	
-	      $elm = $(this);
+	//       $elm = $(this);
 	 
-	      if($elm.hasClass('animated')) { return true; } // if already animated skip to the next item
+	//       if($elm.hasClass('animated')) { return true; } // if already animated skip to the next item
 	 
-	      topcoords = $elm.offset().top; // element's distance from top of page in pixels
+	//       topcoords = $elm.offset().top; // element's distance from top of page in pixels
 	 
-	      if(wintop > (topcoords - (winheight*.75))) {
-	        // animate when top of the window is 3/4 above the element
-	        $elm.addClass('animated');
-	      }
+	//       if(wintop > (topcoords - (winheight*.75))) {
+	//         // animate when top of the window is 3/4 above the element
+	//         $elm.addClass('animated');
+	//       }
 
-	    });
+	//     });
 
-	  } // end animate_elems()
+	//   } // end animate_elems()
 
 	
 
