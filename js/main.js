5429908f14b76cec1379df105dd822e81d9965e1
James Dizon <dizonjerlin@gmail.com>
2023-01-03 12:03:05 -0800

    Updated Responsive Designs

--- js/main.js
+++ js/main.js
@@ -192,11 +192,11 @@ $(function(){
 		
 	}
 
-	var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
+// 	var frmvalidator  = new Validator("contactform");
+// frmvalidator.addValidation("name","req","Please provide your name");
+// frmvalidator.addValidation("email","req","Please provide your email");
+// frmvalidator.addValidation("email","email",
+//   "Please enter a valid email address");
 
 });
 
